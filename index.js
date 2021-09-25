65bc805edfd0cad61c327d6fb51e188f47e4704f
Sayali Desai <dsayali422@gmail.com>
2021-09-25 16:02:28 +0530

    final node-api-js

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
 const express=require("express");
 const app=express();
-const mongoose=require("mongoose");
+const mongoose=require(
+    "mongoose");
 const morgan=require("morgan");
 const helmet=require("helmet");
 const dotenv=require("dotenv");
 const userRoute=require("./routes/users");
 const authRoute=require("./routes/auth");
+const postRoute=require("./routes/posts")
 
 
 dotenv.config();
 
 mongoose.connect(
-    process.env.MONGO_URL,{usenewUrlParser: true, useUnifiedTopology: true},
-    ()=>{
-        console.log("Connected to MongoDB")
-    });
-
+    process.env.MONGO_URL,
+    {
+        useNewUrlParser: true, useUnifiedTopology: true}
+    )
+    .then(()=>console.log("Connected to MongoDB"))
+    .catch(e=>console.log(e));
 //middleware
 app.use(express.json());
 app.use(helmet());
@@ -23,7 +26,7 @@ app.use(morgan("common"));
 
 app.use("/api/users",userRoute);
 app.use("/api/auth",authRoute);
-
+app.use("/api/posts",postRoute);
 
 
 app.listen(8800,()=>{
